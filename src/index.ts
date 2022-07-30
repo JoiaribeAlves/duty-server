0b425da5b0a12dcc927ba367cdae8c18869a7143
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-07-30 01:43:05 -0400

    Importa o arquivo routes.ts

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import express from "express";
 import cors from "cors";
 
 import "./database";
+import router from "./routes";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
-app.get("/", (req, res) => {
-	res.status(200).json({ msg: "Hello world" });
-});
-
-app.listen(port, () => console.log(`Server is running on port: ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
