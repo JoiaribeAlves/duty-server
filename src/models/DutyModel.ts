861dec083b137a7c569363844b7b757320451dd6
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-08-03 21:30:00 -0400

    Adiciona o DutyModel.ts

---
+++ src/models/DutyModel.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+interface IDuty {
+	pharmacyId: string;
+	date: Date;
+	createdAt: Date;
+}
+
+const DutySchema = new mongoose.Schema<IDuty>({
+	pharmacyId: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const Duty = mongoose.model<IDuty>("Duty", DutySchema);
+
+export default Duty;
