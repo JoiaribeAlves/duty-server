7c1467a08515ea3c3d52472d77a13706c73a32bc
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-08-05 18:50:52 -0400

    Transforma as funções em assincronas

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,9 @@
 import bcrypt from "bcryptjs";
 
-export function createPasswordHash(password: string) {
+export async function createPasswordHash(password: string) {
 	return bcrypt.hashSync(password);
 }
 
-export function passwordCompare(password: string, passwordHash: string) {
+export async function passwordCompare(password: string, passwordHash: string) {
 	return bcrypt.compareSync(password, passwordHash);
 }
