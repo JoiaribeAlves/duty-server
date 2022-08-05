8b9716bce198e94a9d876076c6c08c42e3fb7b14
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-08-05 14:25:28 -0400

    Cria o Model de usuário

---
+++ src/models/UserModel.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+interface IUser {
+	email: string;
+	password: string;
+	isAdmin: true | false;
+	createdAt: Date;
+}
+
+const UserSchema = new mongoose.Schema<IUser>({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		select: false,
+	},
+	isAdmin: {
+		type: Boolean,
+		default: false,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const User = mongoose.model<IUser>("User", UserSchema);
+
+export default User;
