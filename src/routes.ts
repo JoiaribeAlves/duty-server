b9ad40df4c53841c8e43aba6dbf3662b62835268
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2023-01-06 17:52:19 -0400

    Altera a mensagem na página inicial.

--- src/routes.ts
+++ src/routes.ts
@@ -11,7 +11,7 @@ const router = Router();
 /* Public Routes */
 // Index
 router.get("/", (req, res) => {
-	res.status(200).json({ message: "OK" });
+	res.status(200).json({ message: "OK!" });
 });
 
 // Pharmacy
