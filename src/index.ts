ea1befd32b145237d1c6cba1fa7baea7976a2e91
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2023-01-26 01:37:46 -0400

    Adiciona configurações do cors.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 
 import "./database";
 import router from "./routes";
@@ -8,13 +8,25 @@ import router from "./routes";
 const app = express();
 const port = process.env.PORT || 4000;
 
+const whiteList = [
+	"https://www.plantaodefarmacia.com.br",
+	"https://api.plantaodefarmacia.com.br",
+	"https://admin.plantaodefarmacia.com.br",
+];
+
+const corsOptions: CorsOptions = {
+	origin: (origin, callback) => {
+		if (whiteList.indexOf(origin!) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Invalid origin."));
+		}
+	},
+	methods: ["GET", "POST", "PATCH", "DELETE"],
+};
+
 app.use(express.json());
-app.use(
-	cors({
-		origin: "*",
-		methods: ["GET", "POST", "PATCH", "DELETE"],
-	})
-);
+app.use(cors(corsOptions));
 app.use(router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
